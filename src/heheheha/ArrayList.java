c919363699dabe6f8def3f8093bc31da0cb36824
Julian Young <julian.young@cuw.edu>
2025-11-21 20:00:30 -0600

    Update V3

--- src/heheheha/ArrayList.java
+++ src/heheheha/ArrayList.java
@@ -2,56 +2,65 @@ package heheheha;
 
 import java.util.Arrays;
 
-public class ArrayList<T extends Comparable<T>> {
+public class ArrayList<T extends Comparable<T>>
+{
 
     private T[] data;
     private int size;
 
     @SuppressWarnings("unchecked")
-    public ArrayList(int capacity) {
+    public ArrayList(int capacity)
+    {
         data = (T[]) new Comparable[capacity];
         size = 0;
     }
 
-    public int size() {
+    public int size()
+    {
         return size;
     }
 
-    public boolean isEmpty() {
+    public boolean isEmpty()
+    {
         return size == 0;
     }
 
-    private void ensureCapacity() {
-        if (size >= data.length) {
+    private void ensureCapacity()
+    {
+        if (size >= data.length)
+        {
             data = Arrays.copyOf(data, data.length * 2);
         }
     }
 
-    public void append(T value) {
+    public void append(T value)
+    {
         ensureCapacity();
         data[size] = value;
         size++;
     }
 
-    public void prepend(T value) {
+    public void prepend(T value)
+    {
         ensureCapacity();
-        // Shift everything right by 1
-        for (int i = size; i > 0; i--) {
+        for (int i = size; i > 0; i--)
+        {
             data[i] = data[i - 1];
         }
         data[0] = value;
         size++;
     }
 
-    public void insertAt(int index, T value) {
+    public void insertAt(int index, T value)
+    {
         if (index < 0 || index > size) {
             throw new IndexOutOfBoundsException("Index: " + index);
         }
 
         ensureCapacity();
 
-        // Shift right from index
-        for (int i = size; i > index; i--) {
+        for (int i = size; i > index; i--)
+        {
             data[i] = data[i - 1];
         }
 
@@ -59,15 +68,17 @@ public class ArrayList<T extends Comparable<T>> {
         size++;
     }
 
-    public T removeAt(int index) {
-        if (index < 0 || index >= size) {
+    public T removeAt(int index)
+    {
+        if (index < 0 || index >= size)
+        {
             throw new IndexOutOfBoundsException("Index: " + index);
         }
 
         T removed = data[index];
 
-        // Shift left
-        for (int i = index; i < size - 1; i++) {
+        for (int i = index; i < size - 1; i++)
+        {
             data[i] = data[i + 1];
         }
 
@@ -76,25 +87,30 @@ public class ArrayList<T extends Comparable<T>> {
         return removed;
     }
 
-    public int search(T value) {
-        for (int i = 0; i < size; i++) {
-            if (data[i].equals(value)) {
+    public int search(T value)
+    {
+        for (int i = 0; i < size; i++)
+        {
+            if (data[i].equals(value))
+            {
                 return i;
             }
         }
         return -1;
     }
 
-    public void sort(boolean ascending) {
-        for (int i = 0; i < size - 1; i++) {
-            for (int j = 0; j < size - i - 1; j++) {
+    public void sort(boolean ascending)
+    {
+        for (int i = 0; i < size - 1; i++)
+        {
+            for (int j = 0; j < size - i - 1; j++)
+            {
 
                 int comparison = data[j].compareTo(data[j + 1]);
 
-                // If ascending AND data[j] > data[j+1], swap
-                // If descending AND data[j] < data[j+1], swap
                 if ((ascending && comparison > 0) ||
-                    (!ascending && comparison < 0)) {
+                    (!ascending && comparison < 0))
+                {
 
                     T temp = data[j];
                     data[j] = data[j + 1];
@@ -104,8 +120,34 @@ public class ArrayList<T extends Comparable<T>> {
         }
     }
 
-    // Optional helper to print
-    public String toString() {
+    public String toString()
+    {
         return Arrays.toString(Arrays.copyOf(data, size));
+    }
+
+    public static void main(String[] args)
+    {
+
+        ArrayList<Integer> list = new ArrayList<>(5);
+
+        list.append(10);
+        list.append(3);
+        list.append(8);
+
+        list.prepend(20);        // [20, 10, 3, 8]
+        list.insertAt(2, 99);    // [20, 10, 99, 3, 8]
+
+        System.out.println("List: " + list);
+
+        list.removeAt(1);        // removes 10
+        System.out.println("After remove: " + list);
+
+        System.out.println("Searching 99: index = " + list.search(99));
+
+        list.sort(true);         // ascending
+        System.out.println("Sorted ascending: " + list);
+
+        list.sort(false);        // descending
+        System.out.println("Sorted descending: " + list);
     }
 }
